81f4055261d471e063c2b5397c5b3f381e60e4c2
Swaraj Gaikwad <swarajgaikwad1925@gmail.com>
2026-06-14 18:21:11 +0530

    Prototype-1

---
+++ _next/static/HrzkSMSCPI8otE20zMC1_/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/NineCoin//_next/:path+",
+        "destination": "/NineCoin/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
